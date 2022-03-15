af3b1bdbbf65b40f28e736be3b7b8f3ee0992aea
jatink-addweb <jatin.k.addweb@gmail.com>
2022-03-15 18:42:12 +0530

    cookiesTemplate

---
+++ 15-03-2022/webpage/login.php
@@ -0,0 +1,82 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+<!--author:starttemplate-->
+<!--reference site : starttemplate.com-->
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div id="login">
+        <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+                            <h3 class="text-center text-info">Login</h3>
+                            <div class="form-group">
+                                <label for="username" class="text-info">Email:</label><br>
+                                <input type="email" class="form-control" name="email" placeholder="Email" required="required"> 
+                  </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                
+                            </div>
+                            <div class="mn">
+                            <input type="submit" name="submit" class="btn btn-info btn-md" name='submit' value="submit">
+                            <?php error_reporting(0); if ($_POST['submit']){ 
+                                if($_POST['email']==$_COOKIE['email'] and $_POST['password']==$_COOKIE['password'])
+                                    {
+                                        header('Location:home.php');
+                                    }
+                                echo "<script>window.alert('username and password not currect !') </script> ";
+                                } ?>
+                                
+                                <input type="submit" name="register" class="btn btn-info btn-md io" value="register">
+                                <?php error_reporting(0); if ($_POST['register']){ header('Location:signup.php');} ?>
+                            </div>
+                           
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<style>
+   body {
+  margin: 0;
+  padding: 0;
+  background-color: #17a2b8;
+  height: 100vh;
+}
+#login .container #login-row #login-column #login-box {
+  margin-top: 120px;
+  max-width: 600px;
+  height: 400px;
+  border: 1px solid #9C9C9C;
+  background-color: #EAEAEA;
+}
+#login .container #login-row #login-column #login-box #login-form {
+  padding: 20px;
+}
+#login .container #login-row #login-column #login-box #login-form #register-link {
+  margin-top: -85px;
+}
+.mn{
+    margin:50px 10px 10px;
+}
+.io{
+    float:right;
+}
+</style>
+</body>
+</html>
